1124af15dbe377f7eb73815553507fd3dd99a01d
mrptashkin <mrsol2010mrsol@gmail.com>
2022-04-25 13:57:56 +0300

    ДЗ #2_1

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -7,9 +7,11 @@ public class CustomerReverseOrder {
 
     private final Deque<Customer> customerList = new LinkedList<>();
 
+
     public void add(Customer customer) {
         customerList.push(customer);
     }
+
 
     public Customer take() {
         return customerList.pop();
